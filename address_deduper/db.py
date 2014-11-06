8846838cc94d56e8b9b40e3de331c377e2b7b22c
Al <albarrentine@gmail.com>
2014-11-06 15:07:24 -0500

    [fix] db dir

--- address_deduper/db.py
+++ address_deduper/db.py
@@ -3,15 +3,13 @@ import shutil
 
 from address_normalizer.deduping.storage.base import *
 
-def cleanup_db_dir(config):
-    db_dir = config['STORAGE_ROCKSDB_DIR']
+def cleanup_db_dir(db_dir):
     if config.get('CLEAN_DB_ON_START', False) and os.path.exists(db_dir):
         shutil.rmtree(db_dir)
     else:
         lock_path = os.path.join(db_dir, 'LOCK')
         if os.path.exists(lock_path):
             os.unlink()
-    return db_dir
 
 def db_from_config(config):
     storage_type = config.get('STORAGE', storage_types.NOP)
@@ -20,10 +18,12 @@ def db_from_config(config):
         return NopStorage()
     elif storage_type == storage_types.LEVELDB:
         import address_normalizer.deduping.storage.level as level
-        db_dir = cleanup_db_dir(config)
+        db_dir = config['STORAGE_LEVELDB_DIR']
+        cleanup_db_dir(db_dir)
         return level.LevelDBNearDupeStorage(level.LevelDB(db_dir))
     elif storage_type == storage_types.ROCKSDB:
         import address_normalizer.deduping.storage.rocks as rocks
         import rocksdb
-        db_dir = cleanup_db_dir(config)
+        db_dir = config['STORAGE_ROCKSDB_DIR']
+        cleanup_db_dir(db_dir)
         return rocks.RocksDBNearDupeStorage(rocksdb.RocksDB(db_dir, rocksdb.Options(create_if_missing=True)))
