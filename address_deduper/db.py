3c70c81e0e9562a21b8888b196fda167cb94cad5
Al <albarrentine@gmail.com>
2014-11-06 15:05:11 -0500

    [fix] cleanup db setup

--- address_deduper/db.py
+++ address_deduper/db.py
@@ -3,6 +3,16 @@ import shutil
 
 from address_normalizer.deduping.storage.base import *
 
+def cleanup_db_dir(config):
+    db_dir = config['STORAGE_ROCKSDB_DIR']
+    if config.get('CLEAN_DB_ON_START', False) and os.path.exists(db_dir):
+        shutil.rmtree(db_dir)
+    else:
+        lock_path = os.path.join(db_dir, 'LOCK')
+        if os.path.exists(lock_path):
+            os.unlink()
+    return db_dir
+
 def db_from_config(config):
     storage_type = config.get('STORAGE', storage_types.NOP)
 
@@ -10,18 +20,10 @@ def db_from_config(config):
         return NopStorage()
     elif storage_type == storage_types.LEVELDB:
         import address_normalizer.deduping.storage.level as level
-        db_dir = config['STORAGE_LEVELDB_DIR']
-        if config.get('CLEAN_DB_ON_START', False) and os.path.exists(db_dir):
-            shutil.rmtree(db_dir)
-        else:
-            os.unlink(os.path.join(db_dir, 'LOCK'))
-
+        db_dir = cleaned_db_dir(config)
         return level.LevelDBNearDupeStorage(level.LevelDB(db_dir))
     elif storage_type == storage_types.ROCKSDB:
         import address_normalizer.deduping.storage.rocks as rocks
-        db_dir = config['STORAGE_ROCKSDB_DIR']
-        if config.get('CLEAN_DB_ON_START', False) and os.path.exists(db_dir):
-            shutil.rmtree(db_dir)
-        else:
-            os.unlink(os.path.join(db_dir, 'LOCK'))
-        return rocks.RocksDBNearDupeStorage(rocks.RocksDB(db_dir))
+        import rocksdb
+        db_dir = cleaned_db_dir(config)
+        return rocks.RocksDBNearDupeStorage(rocksdb.RocksDB(db_dir, rocksdb.Options(create_if_missing=True)))
