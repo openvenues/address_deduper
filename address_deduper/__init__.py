eaa59fac3d8442a9512aa67a5b3e9a719a017a2a
Al <albarrentine@gmail.com>
2014-11-06 14:25:31 -0500

    [init] Adding Flask app to this project and keeping address_normalizer as a library

---
+++ address_deduper/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+
+import .config
+from .db import db_from_config
+
+from address_deduper.views import init_views
+from address_normalizer.
+
+def create_app(env, **kw):
+    app = Flask(__name__)
+    specified_config = kw.get('config')
+    if specified_config:
+        __import__('address_normalizer.' + specified_config)
+    config.current_env = env
+    conf = config.valid_configs.get(env)
+    if not conf:
+        sys.exit('Invalid config, choices are [%s]' % ','.join(valid_configs.keys()))
+
+    app.config.from_object(conf)
+    
+    app.url_map.strict_slashes = False
+
+    db = db_from_config(app.config)
+    AddressNearDupe.configure(db)
+    init_views(app)
+   
+    return app
